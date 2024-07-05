e18c2cdec810f7c166d1ac4f2a30b88b35ebdf95
lewis <lewiskipkemoi53@gmail.com>
2024-07-05 19:07:47 +0300

    all

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.toLowerCase().startsWith(string.toLowerCase());
+    });
+}
+
+
+function matchName(drivers, string) {
+    return drivers.filter(function(driver) {
+        return driver.name.toLowerCase() === string.toLowerCase();
+    });
+}
+
+
+ let drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Sarah', hometown: 'San Francisco' }
+];
+
+// Test cases
+console.log(findMatching(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah'], 'Bobby'));
+console.log(fuzzyMatch(['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah'], 'Sa')); 
+console.log(matchName(drivers, 'Bobby')); 
